273dbd907ccab3cdcbb621076a547600b4bca135
SaraiReyes <idalia_sarai@hotmail.com>
2021-04-16 12:45:11 -0500

    version1.0

---
+++ FundamentosPython.py
@@ -0,0 +1,78 @@
+#pip install -U pylint --user para autoayuda
+
+#imprimir en consola
+print("hola mundo")
+
+# tipos de datos
+
+#texto
+texto="Hola"
+texto2='Hola'
+#boleanos
+falsoVerdadero=False #True || False
+#numeros
+numero=5
+numerodecimal=5.2
+# imprimir número y el tipo de dato que python reconoce
+print(texto, type(texto))
+print(texto2, type(texto2))
+print(falsoVerdadero, type(falsoVerdadero))
+print(numero, type(numero))
+print(numerodecimal, type(numerodecimal))
+
+
+# Operadores
+    # Aritmeticos
+'''
++       suma
+-       resta
+*       multiplicación
+**      potencia
+/       división
+//      división entera
+%       módulo
+
+'''
+n1=4
+n2=56.3
+suma=n1+n2
+resta=n2-n1
+multiplicacion=n1*n2
+division=n2/n1
+divisionEntera=n2//n1
+potencia=5**2
+modulo=9%3
+
+print("la suma es",suma)
+print("la resta es",resta)
+print("la multiplicación es",multiplicacion)
+print("la división es",division)
+print("la división entera es es",divisionEntera)
+print("la potencia es",potencia)
+print("el módulo es",modulo)
+
+
+# variables dinámicas
+dato=50
+print(dato)
+dato="Cambio"
+print(dato)
+
+'''
+Esto tambien es un comentario
+pero este puede ser multilínea
+'''
+
+
+    #relacionales
+'''
+==      igual que
+!=      distinto de
+<       menor que
+<=      menor o igual que
+>       mayor que
+>=      mayor o igual que
+'''
+
+
+
