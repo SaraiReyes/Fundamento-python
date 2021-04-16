4c583c2b8cebe735534fcbd5e31fa9679f745855
SaraiReyes <idalia_sarai@hotmail.com>
2021-04-16 13:45:03 -0500

    version1.1

--- FundamentosPython.py
+++ FundamentosPython.py
@@ -2,7 +2,7 @@
 
 #imprimir en consola
 print("hola mundo")
-
+##########################################################################################################
 # tipos de datos
 
 #texto
@@ -20,7 +20,7 @@ print(falsoVerdadero, type(falsoVerdadero))
 print(numero, type(numero))
 print(numerodecimal, type(numerodecimal))
 
-
+##########################################################################################################
 # Operadores
     # Aritmeticos
 '''
@@ -51,7 +51,7 @@ print("la división entera es es",divisionEntera)
 print("la potencia es",potencia)
 print("el módulo es",modulo)
 
-
+##########################################################################################################
 # variables dinámicas
 dato=50
 print(dato)
@@ -62,8 +62,8 @@ print(dato)
 Esto tambien es un comentario
 pero este puede ser multilínea
 '''
-
-
+##########################################################################################################
+# Operadores
     #relacionales
 '''
 ==      igual que
@@ -73,6 +73,80 @@ pero este puede ser multilínea
 >       mayor que
 >=      mayor o igual que
 '''
+comparacion = 50 == 100
+distinto= 50!=100
+menor=50<100
+menor1=50<=100
+mayor=50>100
+mayor1=50>=100
+print("50 es igual que 100?: ",comparacion)
+print("50 es distinto de 100?: ",distinto)
+print("50 es menor o igual que 100?: ",menor)
+print("50 es menor que 100?: ",menor1)
+print("50 es mayor que 100?: ",mayor)
+print("50 es mayor o igual que 100?: ",mayor1)
+##########################################################################################################
+#operadores 
+    #lógicos
+'''
+and     si se cumplen ambas condiciones
+or      si se cumple alguna de las doscondiciones
+not     negación a una condición
 
+'''
+a=30
+b=50
+c=10
 
+opand=((a<b)and(c<b))
+opor=((a>b)or(c<b))
+opnot=not (((a>b)or(c<b)))
 
+print('(a menor qué b ) y (c menor qué b)',opand)
+print('(a mayor qué b ) o (c menor qué b)',opor)
+print('Negamos el resulatdo de= (a menor qué b ) y (c menor qué b)',opnot)
+##########################################################################################################
+#operadores 
+    #de adignación
+'''
+variable= variable +1
+variavle +=1                suma
+variavle -=1                resta
+variavle *=1                multiplicación
+variavle /=1                división
+variavle **=1               potencia
+variavle %=1                módulo 
+'''
+c=1
+c=c+1
+print('valor ',c)
+c+=1
+print('valor ',c)
+c-=1
+print('valor ',c)
+c*=3
+print('valor ',c)
+c/=3
+print('valor ',c)
+c**=3
+print('valor ',c)
+c%=2
+print('valor ',c)
+##########################################################################################################
+#   datos de salida
+app="pyhton"
+proyecto="nuevo"
+
+print(f"La app en {app} se llamará {proyecto}")
+###########################################################################################################
+#   entrada de datos
+## string
+pedirDatos=input("Dame un nombre: ")
+print("el nombre es ",pedirDatos)
+## número
+pedirNum=int(input("Dame un nombre: "))
+print("el número es ",pedirNum+1)
+
+## decimla
+pedirdec=float(input("Dame un nombre: "))
+print("el número es ",pedirdec+1)
